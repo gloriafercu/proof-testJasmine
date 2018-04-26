b8e774148f9ab4db3ce42a937aa28bf7dca9a878
gloriafercu <gloriafercu@gmail.com>
2018-04-26 11:03:53 +0200

    add test jasmine to the project

---
+++ js/main.js
@@ -0,0 +1,70 @@
+var numbers = {
+	randomize: function() {
+		return (Math.floor(Math.random() * (100 - 1)) + 1);
+	}
+};
+
+function getApplication(randomize) {
+	var randomFunction = randomize;
+	if (typeof(randomize) != 'function') {
+		randomFunction = randomizeDefault;
+	}
+
+	function compareNumbers(randomNumber, userSelectedNumber) {
+		if (isNaN(Number(userSelectedNumber))) {
+			return error();
+		}
+		if (randomNumber > userSelectedNumber) {
+			return bigger();
+		} else if (randomNumber < userSelectedNumber) {
+			return smaller();
+		} else {
+			return win();
+		}
+	}
+
+	function randomizeDefault() {
+		return (Math.floor(Math.random() * (100 - 1)) + 1);
+	}
+
+	function getUserSelectedNumber() {
+		return Number(document.getElementById('randNumberInput').value);
+	}
+
+	function setResult(result) {
+		document.getElementById('result').innerHTML = result;
+	}
+
+	function error() {
+		return "Please introduce an integer";
+	}
+
+	function bigger() {
+		return "The random is bigger";
+	}
+
+	function smaller() {
+		return "The random is smaller";
+	}
+
+	function win() {
+		return "YOU WIN!!!";
+	}
+
+	function start() {
+		var randomNumber = randomFunction();
+		var comparisonTrigger = document.getElementById("compare-numbers-button");
+		comparisonTrigger.addEventListener("click", function() {
+			setResult(compareNumbers(randomNumber, getUserSelectedNumber()))
+		});
+	}
+
+	return {
+		compareNumbers: compareNumbers,
+		start: start,
+		error: error,
+		smaller: smaller,
+		bigger: bigger,
+		win: win
+	}
+};
---
+++ test/spec/TestSpec.js
@@ -0,0 +1,37 @@
+describe('the random number guess game', function(){
+    var app, randomize, randomNumber;
+
+    beforeEach(function(){
+        randomNumber = 10;
+        randomize = function(){ return randomNumber };
+        app = getApplication(randomize);
+    });
+
+    it('compares numbers', function(){
+        expect(app.compareNumbers(10, 20)).toEqual(app.smaller());
+        expect(app.compareNumbers(20, 10)).toEqual(app.bigger());
+        expect(app.compareNumbers(10, 10)).toEqual(app.win());
+    });
+    it('validates that input numbers are correct', function(){
+        expect(app.compareNumbers(10, 'abcd')).toEqual(app.error());
+    });
+
+    it('works in integration with the UI', function(){
+        app.start(randomNumber);
+        simulateThatUserIntroducesValue(20);
+
+        doClick();
+
+        expect(readResult()).toEqual(app.smaller());
+    });
+
+    function simulateThatUserIntroducesValue(val) {
+        document.getElementById('randNumberInput').value = val;
+    }
+    function doClick() {
+        document.getElementById("compare-numbers-button").click();
+    }
+    function readResult() {
+        return document.getElementById('result').innerHTML;
+    }
+});
