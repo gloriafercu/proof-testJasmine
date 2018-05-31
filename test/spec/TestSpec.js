215716101cbab71ce07691632f2fd8ac9d03c3e8
gloriafercu <gloriafercu@gmail.com>
2018-05-31 13:07:46 +0200

    corret msg

--- test/spec/TestSpec.js
+++ test/spec/TestSpec.js
@@ -1,39 +1,41 @@
-describe('the random number guess game', function(){
-    var app, randomize, randomNumber;
+describe('the random number guess game', function() {
+	var app, randomize, randomNumber;
 
-    beforeEach(function(){
-        randomNumber = 10;
-				randomize = randomNumber;
-        // randomize = function(){ return randomNumber };
-        app = getApplication(randomize);
-    });
+	beforeEach(function() {
+		randomNumber = 10;
+		randomize = randomNumber;
+		// randomize = function(){ return randomNumber };
+		app = getApplication(randomize);
+	});
 
-    it('compares numbers', function(){
-				console.log(randomize);
-        expect(app.compareNumbers(10, 20)).toEqual(app.smaller());
-        expect(app.compareNumbers(20, 10)).toEqual(app.bigger());
-        expect(app.compareNumbers(10, 10)).toEqual(app.win());
-    });
-    it('validates that input numbers are correct', function(){
-        expect(app.compareNumbers(10, 'abcd')).toEqual(app.error());
-    });
+	it('compares numbers', function() {
+		console.log(randomize);
+		expect(app.compareNumbers(10, 20)).toEqual(app.smaller());
+		expect(app.compareNumbers(20, 10)).toEqual(app.bigger());
+		expect(app.compareNumbers(10, 10)).toEqual(app.win());
+	});
+	it('validates that input numbers are correct', function() {
+		expect(app.compareNumbers(10, 'abcd')).toEqual(app.error());
+	});
 
-    it('works in integration with the UI', function(){
-        app.start(randomNumber);
-        simulateThatUserIntroducesValue(20);
+	it('works in integration with the UI', function() {
+		app.start(randomNumber);
+		simulateThatUserIntroducesValue(20);
 
-        doClick();
+		doClick();
 
-        expect(readResult()).toEqual(app.smaller());
-    });
+		expect(readResult()).toEqual(app.smaller());
+	});
 
-    function simulateThatUserIntroducesValue(val) {
-        document.getElementById('randNumberInput').value = val;
-    }
-    function doClick() {
-        document.getElementById("compare-numbers-button").click();
-    }
-    function readResult() {
-        return document.getElementById('result').innerHTML;
-    }
+	function simulateThatUserIntroducesValue(val) {
+		document.getElementById('randNumberInput').value = val;
+	}
+
+	function doClick() {
+		document.getElementById("compare-numbers-button").click();
+	}
+
+	function readResult() {
+		return document.getElementById('result').innerHTML;
+	}
 });
